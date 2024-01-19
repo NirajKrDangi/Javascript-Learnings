45d04a4a1ce963515937d17f76e48444ae1db8bd
Niraj Kumar Dangi <102892441+NirajKrDangi@users.noreply.github.com>
2024-01-19 19:11:05 +0530

    Updated

---
+++ 2.ArithmeticOpr.js
@@ -0,0 +1,28 @@
+//==== Arithmetic Operators ====//
+
+// console.log(5 + 5);     // 10
+// console.log(10 - 5);    // 5    
+// console.log(10 / 5);    // 2
+// console.log(10 * 5);    // 50
+// console.log(13 % 5);    // 3
+
+// 👍Increment & Decrement Operator ➕➕ & ➖➖
+
+// (++x)Pre Increment & (--x)Pre Decrement
+// num = 5;
+// myNum = ++num;
+
+// console.log(++num);    // 7                     
+// console.log(--num);    // 5     
+// console.log(++myNum);  // 7
+
+
+// Post Increment(num++) & Post Decrement(num--)
+num = 5;
+myNum = num++; // 5 -> (6)
+
+console.log(num++);    // 6 -> (7)                
+console.log(num--);    // 7 -> (6)  
+console.log(myNum++);  // 5 -> (6)
+console.log(myNum);    // 6 -> (6)
+
