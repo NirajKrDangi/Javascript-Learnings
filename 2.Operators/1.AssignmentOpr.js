faa4d6399427803924b5d919fe60c4684b30e454
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-19 19:08:47 +0530

    updated

---
+++ 2.Operators/1.AssignmentOpr.js
@@ -0,0 +1,13 @@
+//==== Assignment Operators ====//
+
+var x = 5;
+var y = 5;
+
+console.log(`Is x and y are equal or not : ${x == y}`);
+console.log(`Addition of x + y : ${x + y}`);
+console.log(`x - y is equal to =  ${x - y}`);
+
+
+//==== Output =====//
+// Is x and y are equal or not : true
+// Addition of x + y : 10
