278c8eff74dfceefe192ffafb8eb29d23b231bbf
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-20 18:57:25 +0530

    Create 5.Concatenation.js

---
+++ 2.Operators/5.Concatenation.js
@@ -0,0 +1,6 @@
+//==== Concatenation Operator ====//
+// Concatenate two string values together, 
+
+var myName = "Niraj";
+console.log(myName + " Kumar");	// Niraj Kumar
+console.log(myName + " Kumar" + " Dangi");  // Niraj Kumar Dangi
