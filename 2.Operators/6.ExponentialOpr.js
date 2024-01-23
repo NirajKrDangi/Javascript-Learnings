62780b5566fb9c07bf4e5016989a967c30d7ac48
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-23 18:44:28 +0530

    Create 6.ExponentialOpr.js

---
+++ 2.Operators/6.ExponentialOpr.js
@@ -0,0 +1,13 @@
+// JavaScript exponentiation operator
+
+let a = Math.pow(2,3);
+console.log(a);  // 8
+
+// ECMAScript 2016: exponentiation operator
+let b = 2**3;
+console.log(a);  // 8
+
+let c = (-3)**2;
+let d = (-3)**3;
+console.log(c);  // 9
+console.log(d);  // -27
