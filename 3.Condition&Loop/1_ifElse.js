ef7246a20c324787b72a12f0548c0d5ece7a1b0c
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-23 18:47:33 +0530

    Create 1_ifElse.js

---
+++ 3.Condition&Loop/1_ifElse.js
@@ -0,0 +1,30 @@
+//==== if..else Statement ====/
+
+var myAge = 21;
+
+if(myAge >= 18){
+    console.log("Eligible for Voting");
+} else{
+    console.log("Not Eligible for Voting");
+}
+
+
+// Nested if
+
+// let age = 16;
+// let state = "jharkhand";
+
+// if(state = "jharkhand") {
+//     if(age >= 16) {
+//         console.log('You can drive');
+//     }
+// }
+
+
+// Avoid using nested if: you can use the &&
+
+// let age = 16;
+// let state = 'jh';
+// if(age >= 16 && state == 'jh') {
+//     console.log('You can drive in jharkhand');
+// }
