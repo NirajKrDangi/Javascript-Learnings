add0d9280ab841ab6f8bcb0cd8f3bf5f571fc5a2
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-25 18:18:43 +0530

    Create 5_whileLoop.js

---
+++ 3.Condition&Loop/5_whileLoop.js
@@ -0,0 +1,31 @@
+//Program to print 1 - 10 counting number
+// initializing tha variable
+
+// let i = 1;
+
+// while(i <= 10){
+//     console.log(i);
+//     i += 1;
+// }
+
+//---------------------------------------------------------------------------//
+
+// Q1.program to find the sum of positive numbers if the user enters a negative numbers, the loop ends the negative number entered is not added to sum
+
+// let sum = 0; 
+// const prompt=require("prompt-sync")({sigint:true});
+// let num = parseInt(prompt("Enter a number: "));
+
+// while(num >= 0){
+//     sum = sum + num;
+//     num = parseInt(prompt("Enter a number: "));
+// }
+// console.log(`The sum is: ${sum}`);
+
+
+// Odd numbers between 1 and 10 -->
+let count = 1;
+while (count < 10){
+    console.log(count);
+    count +=2; 
+}
