dfeb529b43f1d24f09b058c7169c193d7635d711
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-26 18:51:00 +0530

    Create 1_define&Calling.js

---
+++ 4.Function/1_define&Calling.js
@@ -0,0 +1,19 @@
+// Function
+
+// Define variable
+let a = 5, b = 10;
+
+// Declaring a function
+function sum() {
+    console.log(a + b);
+}
+
+// Calling a function
+sum();
+
+
+// function expression
+const login = function(){console.log("Logged-In")} 
+
+login();
+
