3923791f029929621283b73c1aa4e606895155cf
Niraj Kumar <niraj8825224435@gmail.com>
2024-01-30 20:08:44 +0530

    Create 10_invokedFUnction.js

---
+++ 4.Function/6_anonymousFunction.js
@@ -0,0 +1,41 @@
+// anonymous function: function without name
+// let sumExp = function(a, b){
+//     return a + b;
+// }
+
+// let sum = sumExp(5, 10);
+// let sum1 = sumExp(10, 20);
+// let sum2 = sumExp(2.5, 5.98);
+
+// console.log("Total is ", sum);
+// console.log("Total is ", sum1);
+// console.log("Total is ", sum2);
+
+// console.log(sum1 > sum2); 
+
+
+
+// **** Global Variable **** ==>
+// var a = "Niraj";
+// function name1(){
+//     console.log(a);
+// }
+// name1();
+// console.log(a);
+
+// OUTPUT->
+// Niraj
+// Niraj
+
+
+// **** Local Variable **** ==>
+// function name1(){
+//     var a = "Niraj";
+//     console.log(a);
+// }
+// name1();
+// console.log(a);
+
+// OUTPUT-> 
+// Niraj
+// ReferenceError: a is not defined
