95f7b60877ec335b45e972f0521f16d023569d90
Niraj Kumar <niraj8825224435@gmail.com>
2024-02-01 12:16:57 +0530

    Create 10_invokedFUnction.js

---
+++ 4.Function/10_invokedFUnction.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function
+
+
+// Example: 1
+(function (a, b){
+    console.log(a + b);
+})(5, 10);
+
+
+
+// Example: 2
+(function (a){
+    console.log(`Square of ${a} => ${a * a}`);
+})(5);
