04f6e0b584299ab6be7dc5c6b6fbddcc6f8fb736
Niraj Kumar <niraj8825224435@gmail.com>
2024-02-01 12:19:42 +0530

    Create 12.loop.js

---
+++ 4.Function/12.loop.js
@@ -0,0 +1,25 @@
+// Q1. Print even number from 1 to 20;
+for(let i=1; i<=20; i++){ 
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+// Q2. Print odd number from 1 to 20;
+for(let i=1; i<=10; i++){  
+    if(i%2!=0){
+        console.log(i);
+    }
+}
+
+
+// Q3. Table
+for(let i=1; i<=10; i++){
+    for(let j=1; j<=10; j++){
+        console.writeline(j+"  ");
+    }
+    console.log(" ");
+} 
+
+
