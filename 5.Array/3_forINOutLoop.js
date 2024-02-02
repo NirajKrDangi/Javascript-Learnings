da393fc8fec5ae0a159d5989b6717aed9d96c5fb
Niraj Kumar <niraj8825224435@gmail.com>
2024-02-02 18:17:23 +0530

    Create 3_forINOutLoop.js

---
+++ 5.Array/3_forINOutLoop.js
@@ -0,0 +1,20 @@
+// For..in and For..of Loop: ==>
+const fruit = ["Apple", "Mango", "Grapes", "Banana", "Coconut"];
+
+
+// Without using for..in & for..of -->
+for(i = 0; i < fruit.length; i++){
+    console.log(fruit[i]);
+}
+
+
+// Using for..in loop --> (Enumerating), Mostly used with object.
+// for(let elements in fruit){
+//     console.log(elements);
+// }
+
+
+//Using for..of loop --> (Iterating), Mostly used with array & string.
+// for(let elements of fruit){
+//     console.log(elements);
+// }
