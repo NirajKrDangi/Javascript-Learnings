7c16332571eb1148dbc8b11794ea2ecfe4f47263
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-02-05 22:01:16 +0530

    Create 8.spreedOpr.js

---
+++ 5.Array/8.spreedOpr.js
@@ -0,0 +1,17 @@
+// Add Two Array Value in One
+const arrayOne = [1, 2, 3, 4];
+const arrayTwo = [5, 6, 7, 8];
+
+const arrayThree = [...arrayOne, ...arrayTwo];
+console.log(arrayThree);
+
+
+function one() {
+    console.log(argument);
+}
+one([1, 2, 3, 4]);
+
+
+// Convert Object to Array 
+console.log(Array.from("Niraj"));
+
