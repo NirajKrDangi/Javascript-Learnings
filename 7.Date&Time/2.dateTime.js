12229f14f18fe0eac1c60f2f1ebe62156924ef7c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-02-07 19:08:36 +0530

    Create 2.dateTime.js

---
+++ 7.Date&Time/2.dateTime.js
@@ -0,0 +1,6 @@
+var date = new Date().toLocaleTimeString();
+var time = new Date().toLocaleDateString();
+var dateTime = new Date().toLocaleString();
+console.log(date);
+console.log(time);
+console.log(dateTime);
