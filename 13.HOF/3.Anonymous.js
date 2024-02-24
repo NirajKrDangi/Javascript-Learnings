6cce20cacd764a0da8e8eb99a96a9c729f55555b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-02-24 22:12:26 +0530

    Create 3.Anonymous.js

---
+++ 13.HOF/3.Anonymous.js
@@ -0,0 +1,13 @@
+// ANONYMOUS FUNCTION: A function without name
+
+document.addEventListener("click", function(){
+    console.log("Hello");
+})
+
+
+// Example-2
+let sum = function (a,b) {
+    let sum = a+b;
+    console.log(sum);
+}
+sum(5, 5);
