7b38641616b862a74f02eab298f2332b8fa36b55
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-02-27 18:04:22 +0530

    Create 10.Map.js

---
+++ 13.HOF/10.Map.js
@@ -0,0 +1,4 @@
+var arr = ['Tomato', 'Cabbage', 'Spinach', 'Potato', 'Cauliflower', 'Bell Pepper', 'Lettuce', 'Onion'];
+
+// arr.map((item, index) => {console.log(`${item} Added in Cart in Position ${index}`)});
+arr.map((item, index) => {console.log(`${item.toUpperCase()} Added in Cart in Position ${index}`)});
