2f3f74da97684579af39fab1a77324b1ca8d8730
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-02-29 11:18:55 +0530

    Create 15.Sort.js

---
+++ 13.HOF/15.Sort.js
@@ -0,0 +1,7 @@
+/** Number Shorting */
+var arr = [12, 24, 54, 50, 10, 30];
+console.log(arr.sort((a, b) => a - b));
+
+/** String Sorting */
+const userName = ["Amit", "Niraj", "Suraj", "Biki", "Dinesh"];
+console.log(userName.sort());
