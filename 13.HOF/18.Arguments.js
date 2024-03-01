c6a9bfe2253b2e1865e4b6bd78f6e6be14cb639d
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-03-01 22:44:24 +0530

    Create 18.Arguments.js

---
+++ 13.HOF/18.Arguments.js
@@ -0,0 +1,8 @@
+function manyArguments(){
+    let args = Array.from(arguments);
+    // let finalArr = args.map(e => e*2);
+    // console.log(finalArr);
+    console.log(args);
+};
+manyArguments(1, 2, 3, 4, 5);
+manyArguments(1, 2, 3, 4, 5);
