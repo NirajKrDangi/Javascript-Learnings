686ca9199beca54102e642bf0d95881da629ddf8
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-03-22 22:23:46 +0530

    Create Assignment6.js

---
+++ 17 Error Handling/PW Assignment/Assignment6.js
@@ -0,0 +1,14 @@
+// Using Static Method Add Two Numbers With Calculator Class
+
+class calculator {
+    static add(num1, num2){
+        return num1 + num2;
+    }
+}
+const result = calculator.add(10, 20);
+console.log(result);
+
+/**
+ * :::OUTPUT:::
+ * 30
+ */
